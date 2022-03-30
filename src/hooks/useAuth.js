cb6568ab874fd8226f76194d9640cc6f9aac1260
Luis Berenguer <luis01developer@gmail.com>
2022-03-30 13:57:27 +0200

    implementando el logout

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -36,8 +36,16 @@ function useProviderAuth() {
     }
   };
 
+  const logout = () => {
+    Cookie.remove('token');
+    setUser(null);
+    delete axios.defaults.headers.Authorization;
+    window.location.href = '/login';
+  };
+
   return {
     user,
     signIn,
+    logout,
   };
 }
