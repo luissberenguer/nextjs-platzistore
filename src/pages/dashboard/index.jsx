a70f89cc876635b7af70f1e608e960ca92d11631
Luis Berenguer <luis01developer@gmail.com>
2022-03-30 10:07:17 +0200

    mejora de los colores del chartBar

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -23,7 +23,8 @@ export default function Dashboard() {
         label: 'Categories',
         data: countOcurrences(categoryCount),
         borderWidth: 2,
-        backgroundColor: ['rgb(252 165 165)', 'rgb(103 232 249)', 'rgb(165 180 252)', 'rgb(253 186 116)', 'rgb(252 211 77)', 'rgb(190 242 100)', 'rgb(110 231 183)'],
+        backgroundColor: ['rgb(252 165 165)', 'rgb(253 186 116)', 'rgb(252 211 77)', 'rgb(110 231 183)', 'rgb(103 232 249)', 'rgb(165 180 252)', 'rgb(196 181 253)'],
+        // borderColor: ['rgb(220 38 38)', 'rgb(234 88 12)', 'rgb(202 138 4)', 'rgb(5 150 105)', 'rgb(8 145 178)', 'rgb(147 51 234)'],
       },
     ],
   };
